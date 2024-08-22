25c84c7b38654c973f3a76a9a2da22719a352168
GrlDev3 <153387994+GrlDev3@users.noreply.github.com>
2024-08-22 17:10:29 +0200

    Tetris projekt kezdes

---
+++ TetrisCpp/main.cpp
@@ -0,0 +1,25 @@
+/** Tetris game tutorial from Javier López
+ * https://javilop.com/gamedev/tetris-tutorial-in-c-platform-independent-focused-in-game-logic-for-beginners/
+ *****************************************************************************************
+/* 
+/*
+/* gametuto.com - Javier L�pez L�pez (javilop.com)
+/*
+/*****************************************************************************************
+/*
+/* Creative Commons - Attribution 3.0 Unported
+/* You are free:
+/*	to Share � to copy, distribute and transmit the work
+/*	to Remix � to adapt the work
+/*
+/* Under the following conditions:
+/* Attribution. You must attribute the work in the manner specified by the author or licensor 
+/* (but not in any way that suggests that they endorse you or your use of the work).
+/*
+/*****************************************************************************************/
+
+#include "Pieces.h"
+
+void main (){
+
+};
